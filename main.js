d5cae3cd13ba396c14c4c50d6764ef22db01aaa7
Tomascavadini11 <148654090+TomyC8@users.noreply.github.com>
2024-02-28 15:49:45 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,71 @@
+class ProductManager {
+    static ultId = 0;
+    constructor(){
+        this.products = [];
+    }
+
+    addProduct(title, description, price, thumbnail, code, stock) {
+        if (!title || !description || !price || !thumbnail || !code || !stock){
+            console.log("Todos los campos son obligatorios");
+            return;
+        }
+
+      if(this.products.some(item => item.code === code)) {
+        console.error("Ya existe un producto con ese código.");
+        return;
+      }
+
+      const newProduct = {
+        id: ++ProductManager.ultId,
+        title,
+        description,
+        price,
+        thumbnail,
+        code,
+        stock
+      }
+      //si el ++ lo pongo al principio va a pasar que primero se aumenta el id y despues lo asigna, en cambio si lo pongo al final pasaría al reves.
+
+      this.products.push(newProduct);
+    }
+
+    getProducts(){
+        return this.products;
+    }
+
+    getProductById(id){
+        const product = this.products.find(item => item.id === id);
+
+      if(!product) {
+        console.log("Product not found");
+      } else {
+        console.log("Este es el producto", product);
+      }
+    }
+}
+
+//Testing:
+
+//1)Se creara una instancia de la clase "ProductManager"
+
+const manager = new ProductManager();
+
+//2)Se llamará "getProducts" recien creada la instancia, debe devovler un arreglo vacio.
+
+console.log(manager.getProducts());
+
+manager.addProduct("producto prueba", "este es un producto prueba", 200, "sin imagen", "abc123", 25);
+
+//El objeto debe agregarse satisfactoriamente con un id generado automáticamente sin repetirse.
+
+//4)Se llamará el metodo "getProducts" nuevamente, esta vez debe aparecer el producto recien agregado.
+manager.addProduct("pelotas", "de voley", 1500, "sin imagen", "mks110", 15);
+manager.addProduct("remeras", "de entrenamiento", 2000, "sin imagen", "miz629",30);
+
+console.log(manager.getProducts());
+
+//5)Se evaluará que getProductById devuelva error si no encuentra el producto o el producto en caso de encontrarlo.
+
+manager.getProductById(2);
+//Para demostrar que sucede cuando no lo encuentra.
+manager.getProductById(300);
